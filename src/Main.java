ace7d61d39b2f7ca193ede19b4bba837e73733ea
Terrence <teelt@ymail.com>
2022-10-13 23:09:48 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+       // int 1number = 5;
+       // String return = "house";
+
+     String a = "house";
+     int b;
+     String c;
+
+     System.out.println(b);
+     System.out.println(c);
+
+
+    }
+}
